78ae26ea91057041d1980701fd572cb7e7a916ad
niva83 <niva@dtu.dk>
2019-07-16 21:21:40 +0200

    measurement points optimization added and tested

--- CPT/CPT/CPT.py
+++ CPT/CPT/CPT.py
@@ -1,4 +1,5 @@
 import numpy as np
+from itertools import combinations, product
 
 def check_utm_zone(utm_zone):
         flag = False
@@ -49,17 +50,67 @@ def utm2epsg(utm_zone):
         else:
             return None        
 
+def array_difference(A,B):
+    """
+    Finding which elements in array A are not present in
+    array B.
 
+    Parameters
+    ----------
+    A : ndarray
+        nD array containing data with `float` or `int` type
+    B : ndarray
+        nD array containing data with `float` or `int` type
+    """
+
+    nrows, ncols = A.shape
+    dtype={'names':['f{}'.format(i) for i in range(ncols)],
+           'formats':ncols * [A.dtype]}
+
+    C = np.intersect1d(A.view(dtype), B.view(dtype))
+    if len(C)==0:
+        return A
+
+    D = np.setdiff1d(A.view(dtype), B.view(dtype))
+
+    # This last bit is optional if you're okay with "C" being a structured array...
+    # C = C.view(A.dtype).reshape(-1, ncols)
+    if len(D) == 0:
+        return np.array([])
+
+    D = D.view(A.dtype).reshape(-1, ncols)
+    return D
+
+# THIS FUNCTION SHOULD GO AS STATIC METHOD
 def check_measurements(points):
         if(type(points).__module__ == np.__name__):
                 if (len(points.shape) == 1 and points.shape[0] == 3) or (len(points.shape) == 2 and points.shape[1] == 3):
                     return True
                 else:
                     print('Wrong dimensions!')
+                    print('Measurement positions were not added')
                     return False
         else:
             print('Input is not numpy array!')
+            print('Measurement positions were not added')
             return False
+
+# THIS FUNCTION SHOULD GO AS STATIC METHOD
+def check_lidars(points):
+        if(type(points).__module__ == np.__name__):
+                if (len(points.shape) == 1 and points.shape[0] == 3):
+                    return True
+                else:
+                    print('Wrong dimensions!\nLidar position is described by 3 parameters:\n(1)Easting\n(2)Northing\n(3)Height')
+                    print('Lidar position was not added')
+                    return False
+        else:
+            print('Input is not numpy array!')
+            print('Lidar position was not added')
+            return False
+
+
+
 
 class CPT():
     LANDCOVER_DATA_PATH = ""
@@ -69,6 +120,7 @@ class CPT():
     MAP_EXTENT = 5000 # in m
 
     
+    REP_RADIUS = 500 # in m
     MAX_ELEVATION_ANGLE = 5 # in deg
     MIN_INTERSECTING_ANGLE = 30 # in deg
     AVERAGE_RANGE = 3000 # in m
@@ -83,6 +135,8 @@ class CPT():
         self.flags = {'topography':False, 'landcover':False, 'exclusions': False,    
                                     'viewshed':False, 'elevation_angle': False, 'range': False, 
                                     'intersecting_angle':False, 'measurements_optimized': False,
+                                    'measurements_added': False,'lidar_1_pos':False,'lidar_2_pos':False,
+                                    'map_center_added': False,
                                     'utm':False, 'input_check_pass': False}        
 
 
@@ -120,6 +174,11 @@ class CPT():
 
         
         # GIS layers
+        self.map_center = None
+        self.map_corners = None
+        self.x = None
+        self.y = None
+        self.z = None
         self.mesh = None
         self.topography_layer = None
         self.landcover_layer = None        
@@ -134,37 +193,14 @@ class CPT():
 
         CPT.NO_LAYOUTS += 1
 
+    def set_utm_zone(self, utm_zone):
+        if check_utm_zone(utm_zone):
+            self.utm_zone = utm_zone[:-1]
+            self.grid_code = utm_zone.upper() 
+            self.epsg_code = utm2epsg(utm_zone) 
+            self.flags['utm'] = True
 
-
-    def generate_mesh(self, center):
-            """
-            Generate equally spaced (measurement) points on a horizontal plane.
-
-            Parameters
-            ----------
-            center : ndarray
-                    3D array containing data with `float` or `int` type
-                    corresponding to x, y and z coordinates of the mesh center.
-                    3D array data are expressed in meters.
-            map_extent : int
-                    map extent in x and y in meters.
-            mesh_res : int
-                    mesh resolution for x and y in meters.
-            """
-            map_corners = np.array([center[:2] - self.MAP_EXTENT, center[:2] + self.MAP_EXTENT])
-
-            x, y = np.meshgrid(
-                    np.arange(map_corners[0][0], map_corners[1][0] + self.MAP_EXTENT, self.MESH_RES),
-                    np.arange(map_corners[0][1], map_corners[1][1] + self.MAP_EXTENT, self.MESH_RES)
-                            )
-            
-            z = np.full(x.shape, center[2])		
-            mesh = np.array([x, y, z]).T.reshape(-1, 3)
-            return x, y, mesh
-
-    def add_measurements(self, **kwargs):
-        
-
+    def utm_exists(self, **kwargs):
         if (self.utm_zone == None) and (not 'utm_zone' in kwargs):
             print('UTM zone not specified!')
             self.flags['utm'] = False
@@ -175,53 +211,130 @@ class CPT():
                 self.epsg_code = utm2epsg(kwargs['utm_zone']) 
                 self.flags['utm'] = True
         
+    def add_measurements(self, **kwargs):
+        self.utm_exists(**kwargs)
+        if self.flags['utm'] == False:
+            print('Cannot add measurement points without specificing UTM zone!')
         if self.flags['utm'] and check_measurements(kwargs['measurements']):
-
-
-            if len(kwargs['measurements']) == 2:
+            if len(kwargs['measurements'].shape) == 2:
                     self.measurements_initial = np.unique(kwargs['measurements'], axis=0)
             else:
                     self.measurements_initial = np.array([kwargs['measurements']])
                     self.measurements_initial = np.unique(self.measurements_initial, axis=0)
+            self.flags['measurements_added'] = True
 
+    def generating_disc_matrix(self):
+        if self.flags['measurements_added']:
+            points_combination = np.asarray(list(combinations(list(self.measurements_initial[:,(0,1)]), 2)))    
+            discs = (points_combination[:,0] + points_combination[:,1]) / 2
 
-    # def add_lidars(self, **kwargs):
-    #         """
-    #         adding lidar positions points
-    #         *kwargs:
-    #         - lidar_pos_1
-    #         - lidar_pos_2
-
-    #         must find closes index for lidar position...
-    #         """
-
-    #     pass
-
-    # def generate_mesh(self, center, map_extent):
-    #         """
-    #         Generate equally spaced (measurement) points on a horizontal plane.
-
-    #         Parameters
-    #         ----------
-    #         center : ndarray
-    #                 3D array containing data with `float` or `int` type
-    #                 corresponding to x, y and z coordinates of the mesh center.
-    #                 3D array data are expressed in meters.
-    #         map_extent : int
-    #                 map extent in x and y in meters.
-    #         mesh_res : int
-    #                 mesh resolution for x and y in meters.
-    #         """
-    #         map_corners = np.array([center[:2] - map_extent, center[:2] + map_extent])
-
-    #         x, y = np.meshgrid(
-    #                 np.arange(map_corners[0][0], map_corners[1][0]+mesh_res, mesh_res),
-    #                 np.arange(map_corners[0][1], map_corners[1][1]+mesh_res, mesh_res)
-    #                         )
+            temp = np.asarray(list(product(list(discs), list(self.measurements_initial[:,(0,1)]))))
+            distances =  np.linalg.norm(temp[:,0] - temp[:,1], axis = 1)
+            distances = np.where(distances <= self.REP_RADIUS, 1, 0)
             
-    #         z = np.full(x.shape, center[2])		
-    #         mesh = np.array([x, y, z]).T.reshape(-1, 3)
-    #         return x, y, mesh
+            matrix = np.asarray(np.split(distances,len(discs)))
+            total_covered_points = np.sum(matrix,axis = 1)
+
+            matrix = matrix[(-1*total_covered_points).argsort()]
+            discs = discs[(-1*total_covered_points).argsort()]
+
+            # adding 0 m for elevation of each disc
+            discs = np.append(discs.T, np.array([np.zeros(len(discs))]),axis=0).T
+
+            return discs, matrix
+        else:
+            print("No measurement positions added, nothing to optimize!")
+
+    def optimize_measurements(self):
+
+        if self.flags['measurements_added']:
+            discs, matrix = self.generating_disc_matrix()
+            points_uncovered = self.measurements_initial
+            points_covered_total = np.zeros((0,3))
+            discs_selected = np.zeros((0,3))
+            i = 0
+            j = len(points_uncovered)
+
+            while i <= (len(discs) - 1) and j > 0 :
+                indexes = np.where(matrix[i] == 1 )
+                points_covered = self.measurements_initial[indexes]
+                points_new = array_difference(points_covered, points_covered_total)
+                if len(points_new) > 0:
+                    points_covered_total = np.append(points_covered_total, points_new,axis=0)
+                    discs_selected = np.append(discs_selected, np.array([discs[i]]),axis=0)
+                    print(discs_selected)
+                points_uncovered = array_difference(points_uncovered, points_covered)        
+                i += 1
+                j = len(points_uncovered)
+            if len(points_uncovered) > 0:
+                self.measurements_optimized = np.append(discs_selected, points_uncovered, axis = 0)
+            else:
+                self.measurements_optimized = discs_selected
+        else:
+            print("No measurement positions added, nothing to optimize!")
+            
+
+    def add_lidars(self, **kwargs):
+        if 'lidar_1_pos' in kwargs or 'lidar_2_pos' in kwargs:
+            self.utm_exists(**kwargs)
+            if self.flags['utm'] == False:
+                print('Cannot add lidar positions without specificing UTM zone!')
+            else:        
+                if 'lidar_1_pos' in kwargs and check_lidars(kwargs['lidar_1_pos']):
+                    self.lidar_1_pos = kwargs['lidar_1_pos']
+                    self.flags['lidar_1_pos'] = True
+                    print('Lidar 1 position added!')
+                if 'lidar_2_pos' in kwargs and check_lidars(kwargs['lidar_2_pos']):
+                    self.lidar_2_pos = kwargs['lidar_2_pos']
+                    self.flags['lidar_2_pos'] = True
+                    print('Lidar 2 position added!')
+        else:
+            print('Lidar position(s) not specified!')        
+
+    def generate_mesh(self, **kwargs):
+            """
+            Generate equally spaced (measurement) points on a horizontal plane.
+
+            Parameters
+            ----------
+            center : ndarray
+                    3D array containing data with `float` or `int` type
+                    corresponding to Easting, Northing and Height coordinates of the mesh center.
+                    3D array data are expressed in meters.
+            map_extent : int
+                    map extent in Easting and Northing in meters.
+            mesh_res : int
+                    mesh resolution for Easting and Northing in meters.
+            """
+
+            if 'map_extent' in kwargs:
+                self.MAP_EXTENT = kwargs['map_extent']
+            if 'map_center' in kwargs:
+                self.map_center = kwargs['map_center']
+                self.flags['map_center_added'] = True
+            elif self.flags['measurements_added']:
+                self.map_center = np.int_(np.mean(self.measurements_initial,axis = 0))
+                self.flags['map_center_added'] = True
+            else:
+                print('Map center missing!')
+                print('Mesh cannot be generated!')
+                self.flags['map_center_added'] = False
+
+            if self.flags['map_center_added']:
+                # securing that the input parameters are int 
+                self.map_center = np.int_(self.map_center)
+                self.MAP_EXTENT = int(int(self.MAP_EXTENT / self.MESH_RES) * self.MESH_RES)
+                self.map_corners = np.array([self.map_center[:2] - self.MAP_EXTENT, 
+                                             self.map_center[:2] + self.MAP_EXTENT])
+
+                self.x, self.y = np.meshgrid(
+                        np.arange(self.map_corners[0][0], self.map_corners[1][0] + self.MAP_EXTENT, self.MESH_RES),
+                        np.arange(self.map_corners[0][1], self.map_corners[1][1] + self.MAP_EXTENT, self.MESH_RES)
+                                )
+                
+                self.z = np.full(self.x.shape, self.map_center[2])		
+                self.mesh = np.array([self.x, self.y, self.z]).T.reshape(-1, 3)
+
 
     # def optimize_measurements(self):
     #         """
--- CPT/test.py
+++ CPT/test.py
@@ -1,4 +1,141 @@
 # from CPT.CPT import CPT as CPT
 from CPT.CPT import *
+import numpy as np
+from itertools import combinations, product
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle, Wedge, Polygon
 
-test = CPT(utm_zone = '23W')
+MY_DPI = 96
+def measurement_optimization_plt(DPI=100):
+    fig, ax = plt.subplots(sharey=True,figsize=(500/MY_DPI, 500/MY_DPI), dpi=DPI) # note we must use plt.subplots, not plt.subplot
+
+    for i in range(0,len(points)):
+        if i == 0:
+            ax.scatter(points[i][0] - map_center[0], 
+                       points[i][1] - map_center[1], 
+                       marker='o', color='black', s=10,zorder=1000, label = "measurements")
+        else:
+            ax.scatter(points[i][0] - map_center[0], 
+                       points[i][1] - map_center[1], 
+                       marker='o', color='black', s=10,zorder=1000)            
+
+
+    for i in range(0,len(discs)):
+        if i == 0:
+            ax.scatter(discs[i][0] - map_center[0], 
+                       discs[i][1] - map_center[1], 
+                       marker='o', color='red', s=2,zorder=1000, label = "discs")
+            ax.add_artist(plt.Circle((discs[i][0] - map_center[0], discs[i][1] - map_center[1]), 
+                                     REPRESENT_RADIUS,                               
+                                     facecolor='grey', edgecolor='black', 
+                                     zorder=500,  alpha = 0.5))                 
+            
+
+        else:
+            ax.scatter(discs[i][0] - map_center[0], 
+                       discs[i][1] - map_center[1], 
+                       marker='o', color='red', s=2,zorder=1000)
+            ax.add_artist(plt.Circle((discs[i][0] - map_center[0], discs[i][1] - map_center[1]), 
+                                     REPRESENT_RADIUS,                               
+                                     facecolor='grey', edgecolor='black', 
+                                     zorder=500, alpha = 0.5))                       
+             
+
+    plt.xlabel('Easting [m]', fontsize=12)
+    plt.ylabel('Northing [m]', fontsize=12)
+    ax.legend(loc='lower left', fontsize=10)
+
+    ax.set_xlim(-10,10)
+    ax.set_ylim(-10,10)
+
+    ax.set_aspect(1.0)
+    plt.show()
+
+
+
+
+
+
+# def generating_disc_matrix(points,radius):
+    
+#     points_combination = np.asarray(list(combinations(list(points[:,(0,1)]), 2)))    
+#     discs = (points_combination[:,0] + points_combination[:,1]) / 2
+
+#     temp = np.asarray(list(product(list(discs), list(points[:,(0,1)]))))
+#     distances =  np.linalg.norm(temp[:,0] - temp[:,1], axis = 1)
+#     distances = np.where(distances <= radius, 1, 0)
+    
+#     matrix = np.asarray(np.split(distances,len(discs)))
+#     total_covered_points = np.sum(matrix,axis = 1)
+
+#     matrix = matrix[(-total_covered_points).argsort()]
+#     discs = discs[(-total_covered_points).argsort()]
+
+#     # adding 0 m for elevation of each disc
+#     discs = np.append(discs.T, np.array([np.zeros(len(discs))]),axis=0).T
+
+#     return discs, matrix
+
+# def disc_covering(points, radius):
+#     discs, matrix = generating_disc_matrix(points,radius)
+#     points_uncovered = points
+#     points_covered_total = np.zeros((0,3))
+#     discs_selected = np.zeros((0,3))
+#     i = 0
+#     j = len(points_uncovered)
+
+#     while i <= (len(discs) - 1) and j > 0 :
+#         indexes = np.where(matrix[i] == 1 )
+#         points_covered = points[indexes]
+#         points_new = array_difference(points_covered, points_covered_total)
+#         if len(points_new) > 0:
+#             points_covered_total = np.append(points_covered_total, points_new,axis=0)
+#             discs_selected = np.append(discs_selected, np.array([discs[i]]),axis=0)
+#         points_uncovered = array_difference(points_uncovered, points_covered)        
+#         i += 1
+#         j = len(points_uncovered)
+#     discs_selected = np.append(discs_selected, points_uncovered, axis = 0)
+    
+
+
+#     print(points_uncovered)
+#     print(points_covered_total)
+#     # print(points_covered_total)
+#     print("Selected discs:")
+#     print(discs_selected)
+        
+    
+
+map_center = [0,0]
+REPRESENT_RADIUS = 5
+points = np.array([[1,3,3],[2,1,4],[5,2,1],[0,7.4,1],[7.4,4,1]])
+new = CPT()
+new.set_utm_zone("31V")
+new.add_measurements(measurements = points)
+new.REP_RADIUS = REPRESENT_RADIUS
+new.optimize_measurements()
+# print(new.measurements_optimized)
+
+discs = new.measurements_optimized
+measurement_optimization_plt()
+
+# print(new.measurements_initial)
+# print(new.measurements_optimized)
+
+# # discs, matrix = generating_disc_matrix(points, REPRESENT_RADIUS)
+# # print(discs)
+# # disc_covering(points, REPRESENT_RADIUS)
+# # print(matrix.shape)
+# # print(discs)
+
+# disc_covering(points, REPRESENT_RADIUS)
+# print(points)
+# print("\n")
+# print(array_difference(points, np.array([[1,3,3],[2,1,4],[2,2,2]])))
+
+
+# mesh_center = None
+
+# if mesh_center == None:
+#     print('It is None!')  
+
