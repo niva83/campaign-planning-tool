729b91b294124d10913bc9a1559760459205a080
niva83 <niva@dtu.dk>
2019-09-26 19:09:53 +0200

    small change in setup.py

--- setup.py
+++ setup.py
@@ -138,7 +138,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['rasterio', 'pandas', 'geopandas', 'whitebox', 'srtm.py', 'simplekml', 'matplotlib', 'pillow', 'pyyaml', 'dicttoxml','pylint','pathlib','jupyter']
+    install_requires=['numpy', 'pandas', 'geopandas', 'whitebox', 'srtm.py', 'simplekml', 'matplotlib', 'pillow', 'pyyaml', 'dicttoxml','pylint','pathlib','jupyter', 'rasterio']
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
